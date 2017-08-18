019a711f3c63b7fa064cf16f3d4b66b6668b507f
onlykshitij <30444410+onlykshitij@users.noreply.github.com>
2017-08-18 17:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one',function(req, res) {
-   res.send('/Article 1 requested and will be served here'); 
+   res.send('Article 1 requested and will be served here'); 
 });
 
 app.get('/article_two',function(req, res) {
-   res.send('/Article 2 requested and will be served here');
+   res.send('Article 2 requested and will be served here');
 });
 
 app.get('/article_three',function(req, res) {
-   res.send('/Article 3 requested and will be served here');
+   res.send('Article 3 requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
